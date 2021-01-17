426170c5d9a9e917b33792507eec9cafe1044a5f
Fiona Weaver <fiona.l.weaver@gmail.com>
2021-01-17 14:49:33 -0700

    commit tree 6cc7dcd4aba2aabe6e02d0953a2574da3863996c

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,56 @@
 Rails.application.routes.draw do
+  resources :artists, only: [:index, :show, :new, :create, :edit, :update]
+  resources :songs, only: [:index, :show, :new, :create, :edit, :update]
+  resources :genres, only: [:index, :show, :new, :create, :edit, :update]
+  # get 'genres/new'
+
+  # get 'genres/create'
+
+  # get 'genres/edit'
+
+  # get 'genres/update'
+
+  # get 'genres/destroy'
+
+  # get 'genres/index'
+
+  # get 'genres/show'
+
+  # get 'artists/new'
+
+  # get 'artists/create'
+
+  # get 'artists/edit'
+
+  # get 'artists/update'
+
+  # get 'artists/destroy'
+
+  # get 'artists/index'
+
+  # get 'artists/show'
+
+  # get 'songs/new'
+
+  # get 'songs/create'
+
+  # get 'songs/edit'
+
+  # get 'songs/update'
+
+  # get 'songs/destroy'
+
+  # get 'songs/index'
+
+  # get 'songs/show'
+
+  # get 'admin/dashboard'
+
+  # get 'admin/stats'
+
+  # get 'admin/financials'
+
+  # get 'admin/settings'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
